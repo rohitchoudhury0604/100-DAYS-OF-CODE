5b68c844ff36a64a2b4be3f2eb23d278bc9aec00
rohitchoudhury0604 <rohitchoudhury0604@gmail.com>
2025-10-13 02:28:28 +0530

    Create DAY_15_B_REVERSE_A_NUMBER.c

---
+++ DAY_15_B_REVERSE_A_NUMBER.c
@@ -0,0 +1,20 @@
+// Write a program to reverse a given number.
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n, rev = 0, digit;
+    scanf("%d", &n);
+
+    while (n > 0)
+    {
+        digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+    }
+
+    printf("%d\n", rev);
+    return 0;
+}
