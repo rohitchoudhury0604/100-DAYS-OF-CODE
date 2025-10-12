2c43728e7cb84330abfb1d31f15c425644723ba2
rohitchoudhury0604 <rohitchoudhury0604@gmail.com>
2025-10-13 02:34:48 +0530

    Create DAY_19_B_SUM_OF_DIGITS.c

---
+++ DAY_19_B_SUM_OF_DIGITS.c
@@ -0,0 +1,17 @@
+// Write a program to find the sum of digits of a number.
+
+
+#include <stdio.h>
+int main()
+{
+    int n, sum = 0, digit;
+    scanf("%d", &n);
+    while (n > 0)
+    {
+        digit = n % 10;
+        sum = sum + digit;
+        n = n / 10;
+    }
+    printf("%d\n", sum);
+    return 0;
+}
