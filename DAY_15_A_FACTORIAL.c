3f814309c8f10528f722a89c5a7fb2c3506a0489
rohitchoudhury0604 <rohitchoudhury0604@gmail.com>
2025-10-13 02:27:58 +0530

    Create DAY_15_A_FACTORIAL.c

---
+++ DAY_15_A_FACTORIAL.c
@@ -0,0 +1,17 @@
+// Write a program to calculate the factorial of a number.
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int fact = 1;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    printf("%d\n", fact);
+    return 0;
+}
