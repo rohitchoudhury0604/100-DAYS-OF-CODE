6562e4945a9062cafad2dd7c46193da4f160f0da
rohitchoudhury0604 <rohitchoudhury0604@gmail.com>
2025-10-13 02:35:25 +0530

    Create DAY_20_A_PRODUCT_OF_ODD_DIGITS.c

---
+++ DAY_20_A_PRODUCT_OF_ODD_DIGITS.c
@@ -0,0 +1,24 @@
+//Write a program to find the product of odd digits of a number.
+
+
+#include <stdio.h>
+int main() 
+{
+    int n, digit, product = 1, found = 0;
+    scanf("%d", &n);
+    while(n > 0) 
+    {
+        digit = n % 10;
+        if(digit % 2 == 1) 
+        {  
+            product = product * digit;
+            found = 1;
+        }
+        n = n / 10;
+    }
+    if(found)
+        printf("%d\n", product);
+    else
+        printf("1\n");  
+    return 0;
+}
